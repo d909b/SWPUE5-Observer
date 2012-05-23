08e42f716f57332be81a0cd7a374c0b91d40a02a
Patrick Schwab <patrick.schwab@technikum-wien.at>
2012-05-23 12:37:46 +0200

    Initial Commit

---
+++ SWPUE5-Observer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SWPUE5-Observer
+//
+//  Created by Patrick Schwab on 5/23/12.
+//  Copyright (c) 2012 Patrick Schwab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
